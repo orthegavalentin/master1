24a2fc7b4feaa055d9b0313743d9ca772cc294eb
noe <noe.lephilippe@gmail.com>
2015-01-25 05:09:04 +0100

    optimisation bwt

--- infotheory/scheme-compressor/b-w.rkt
+++ infotheory/scheme-compressor/b-w.rkt
@@ -8,17 +8,17 @@
 (provide get-index)
 
 (define (bwt s)
-  (letrec ([size (length s)] [v (make-vector size)])
+  (letrec ([size (length s)] [v (make-vector size)] [s-vec (list->vector s)])
     (for ([i (in-range size)])
       (vector-set! v i i))
-    (last-column (sort (vector->list v) (lambda (x y) (compare x y s size))) s size)))
+    (last-column (sort (vector->list v) (lambda (x y) (compare x y s-vec size))) s-vec size)))
 
 (define (compare x y l size)
   (let f ([x x] [y y])
     (if (eq? (* 2 size) x)
         #f
-        (let ([x-byte (list-ref l (modulo x size))] [y-byte (list-ref l (modulo y size))])
-          (if (eq? x-byte y-byte)
+        (let ([x-byte (vector-ref l (modulo x size))] [y-byte (vector-ref l (modulo y size))])
+          (if (= x-byte y-byte)
               (f (+ 1 x) (+ 1 y))
               (< x-byte y-byte))))))
 
@@ -27,17 +27,17 @@
     (list (map (lambda (i)
                  (when (eq? i 0)
                    (set! index (- size (length (member i l)))))
-                 (list-ref s (modulo (- (+ i size) 1) size))) l) index)))
+                 (vector-ref s (modulo (- (+ i size) 1) size))) l) index)))
 
 (define (make-precedings s)
   (let ([v (make-vector 256 0)])
-    (list->vector (map (lambda (i)
-                         (vector-set! v i (+ 1 (vector-ref v i)))
-                         (list i (- (vector-ref v i) 1)))
-                       s))))
+    (vector-map (lambda (i)
+                  (vector-set! v i (+ 1 (vector-ref v i)))
+                  (list i (- (vector-ref v i) 1)))
+                s)))
 
 (define (make-less-than s)
-  (let ([sorted (sort-data s)] [lt (make-vector 256 -1)])
+  (let ([sorted (list->vector (sort s <))] [lt (make-vector 256 -1)])
     (for ([i (in-range (vector-length sorted))])
       (let ([x (vector-ref sorted i)])
         (when (eq? (vector-ref lt x) -1)
@@ -53,16 +53,17 @@
             (insert heap (car s))
             (f (cdr s)))))))
 
-(define (reverse-bwt s first)
-  (let ([prec (make-precedings s)] [lt (make-less-than s)])
-    (let f ([reversed '()] [last first])
-      (if (eq? (length s) (length reversed))
-          (list->bytes reversed)
+(define (reverse-bwt s first out)
+  (letrec ([size (length s)] [vec (make-vector size)] [prec (make-precedings (list->vector s))] [lt (make-less-than s)])
+    (let f ([n 0] [last first])
+      (if (= size n)
+          (write-bytes (list->bytes (vector->list vec)) out)
           (letrec ([x (vector-ref prec last)]
                    [current (+ (cadr x) (vector-ref lt (car x)))])
             (let ([last-char (vector-ref prec last)])
               (vector-set! prec last (list (car last-char) (- (cadr last-char) 1))))
-            (f (append (list (car x)) reversed) current))))))
+            (vector-set! vec (- size n 1) (car x))
+            (f (+ 1 n) current))))))
 
 (define (write-result l out)
   (if (> 8 (length l))
@@ -88,7 +89,5 @@
     (car x)))
 
 (define (d-bwt out s index)
-  (write-bytes (reverse-bwt s index) out)
+  (reverse-bwt s index out)
   (close-output-port out))
-
-(c-bwt (open-input-file "/home/noe/Téléchargements/test.txt" #:mode 'binary) (current-output-port))
--- infotheory/scheme-compressor/rouleau.rkt
+++ infotheory/scheme-compressor/rouleau.rkt
@@ -6,27 +6,37 @@
 (require "b-w.rkt")
 (require "move-to-front.rkt")
 
-(define (compress-file input output)
+(define (compress-file input output bwt)
   (compress
    (open-input-file input #:mode 'binary)
    (open-output-file "temp.bin" #:mode 'binary #:exists 'replace))
-  
-  (h-compress (open-input-file "temp.bin" #:mode 'binary)
-              (open-input-file "temp.bin" #:mode 'binary)
-              (open-output-file output #:mode 'binary #:exists 'replace))
+  (let ([output (open-output-file output #:mode 'binary #:exists 'replace)])
+    (write-byte bwt output)
+    (h-compress (open-input-file "temp.bin" #:mode 'binary)
+                (open-input-file "temp.bin" #:mode 'binary)
+                output))
   (delete-file "temp.bin"))
 
 (define (decompress-file input output)
-  (h-decompress (open-input-file input #:mode 'binary)            
-                (open-output-file "temp.bin" #:mode 'binary #:exists 'replace))
-  (decompress 
-   (open-input-file "temp.bin" #:mode 'binary)
-   (open-output-file output #:mode 'binary #:exists 'replace))
-  (delete-file "temp.bin"))
+  (letrec ([in (open-input-file input #:mode 'binary)]
+           [bwt (read-byte in)]
+           [out-lzw (if (= bwt 0) output "temp1.bin")])
+    (display "bwt : ")
+    (displayln bwt)
+    (h-decompress in (open-output-file "temp.bin" #:mode 'binary #:exists 'replace))
+    (decompress 
+     (open-input-file "temp.bin" #:mode 'binary)
+     (open-output-file out-lzw #:mode 'binary #:exists 'replace))
+    (when (= 1 bwt)
+      (decompress-bwt "temp1.bin" output)
+      (delete-file "temp1.bin"))
+    (delete-file "temp.bin")))
 
 (define (compress-bwt input output)
-  (let ([output (open-output-file output #:mode 'binary #:exists 'replace)])
-    (c-mtf (c-bwt (open-input-file input #:mode 'binary) output) output)))
+  (let ([out (open-output-file "temp1.bin" #:mode 'binary #:exists 'replace)])
+    (c-mtf (c-bwt (open-input-file input #:mode 'binary) out) out))
+  (compress-file "temp1.bin" output 1)
+  (delete-file "temp1.bin"))
 
 (define (decompress-bwt input output)
   (letrec ([in (open-input-file input #:mode 'binary)] [index (get-index in)])
@@ -35,8 +45,7 @@
 (define (display-help)
   (displayln "rouleau -c[ompress] input_file output_file")
   (displayln "rouleau -d[ecompress] input_file output_file")
-  (displayln "rouleau -b[urrows-wheeler] input_file output_file")
-  (displayln "rouleau -i[nverse-burrows-wheeler] input_file output_file"))
+  (displayln "rouleau -b[urrows-wheeler] input_file output_file"))
 
 (define (main)
   (let ([args (current-command-line-arguments)])
@@ -45,7 +54,7 @@
       [(or
         (equal? (vector-ref args 0) "-c")
         (equal? (vector-ref args 0) "-compress"))
-       (compress-file (vector-ref args 1) (vector-ref args 2))]
+       (compress-file (vector-ref args 1) (vector-ref args 2) 0)]
       [(or
         (equal? (vector-ref args 0) "-d")
         (equal? (vector-ref args 0) "-decompress"))
@@ -54,14 +63,10 @@
         (equal? (vector-ref args 0) "-b")
         (equal? (vector-ref args 0) "-burrows-wheeler"))
        (compress-bwt (vector-ref args 1) (vector-ref args 2))]
-      [(or
-        (equal? (vector-ref args 0) "-i")
-        (equal? (vector-ref args 0) "-inverse-burrows-wheeler"))
-       (decompress-bwt (vector-ref args 1) (vector-ref args 2))]
       [else (display-help)])))
 
-;(main)
+(main)
 
-(compress-bwt "/home/noe/Téléchargements/test.txt" "/home/noe/Téléchargements/out.bin")
-(display "compressed")
-(decompress-bwt "/home/noe/Téléchargements/out.bin" "/home/noe/Téléchargements/out.txt")
+;(compress-bwt "/home/noe/Téléchargements/test.txt" "/home/noe/Téléchargements/out.bin")
+;(display "compressed")
+;(decompress-bwt "/home/noe/Téléchargements/bwt.bin" "/home/noe/Téléchargements/out.txt")
