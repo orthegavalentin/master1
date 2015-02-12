d7ae3de98694714f5f79f461ad4811313a304982
Noé LE PHILIPPE <znoraka@gmail.com>
2015-02-12 11:15:58 +0100

    optimisations

--- algo_exploration_mouvement/parser.rkt
+++ algo_exploration_mouvement/parser.rkt
@@ -13,7 +13,7 @@
 (define (read-file in)
   (let f ()
     (let ([line (read-line in)])
-      (if (eq? (string-ref line 0) #\c)
+      (if (or (eof-object? line) (eq? (string-ref line 0) #\c))
           (f)
           (let ([l (string-split line)])
             (list (string->number (caddr l)) (string->number (cadddr l)) (make-clauses in)))))))
