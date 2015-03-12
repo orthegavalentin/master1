0852fef2a7f72227152ea452a22c7c1e639ecb04
noe <noe.lephilippe@gmail.com>
2015-03-12 19:40:53 +0100

    compte rendu TP6 traitement d'images

--- traitement_image/TP6/main.cpp
+++ traitement_image/TP6/main.cpp
@@ -3,11 +3,13 @@
 
 
 int main() {
-	RawReader *r = new RawReader("/auto_home/nlephilippe/Téléchargements/WHATISIT/whatisit.301x324x56.1.1.1.4.img",
+	RawReader *r = new RawReader("/home/noe/Téléchargements/WHATISIT/whatisit.301x324x56.1.1.1.4.img",
 		301, 324, 56);
 	std::cout << "min : " << r->getMinValue() << std::endl;
 	std::cout << "max : " << r->getMaxValue() << std::endl;
 
 	std::cout << "at 200 200 20 : " << r->getValue(200, 200, 20) << std::endl;
-	r->flatten("out.0.raw");
+	r->aip("aip.0.raw");
+	r->mip("mip.0.raw");
+	r->minip("minip.0.raw");
 }
