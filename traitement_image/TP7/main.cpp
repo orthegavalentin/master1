085918de7a6f1d19b8ecc007c480416c28b62599
nlephilippe <nlephilippe@si9.etu.info-ufr.univ-montp2.fr>
2015-03-13 11:37:43 +0100

    ajout TP7 image avec un smiley

---
+++ traitement_image/TP7/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "RawReader.h"
+
+
+int main() {
+	RawReader *r = new RawReader("/auto_home/nlephilippe/Téléchargements/engine/engine.256x256x128.1x1x1.img",
+		256, 256, 128);
+	r->marchingCubes(1, 1, 1, 200, "");
+}
